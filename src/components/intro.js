551c8dfb5c50aec282243d7a4aeefc4d2dcb5e34
Amr <amruser97@gmail.com>
2023-05-05 22:29:33 +0300

    fixed link bug

--- src/components/intro.js
+++ src/components/intro.js
@@ -11,7 +11,7 @@ export default function Intro(){
       <p className={classes}>I'M <span>Amr Abdelghany</span><br/>Software developer with a passion for web development.
       <br/> I enjoy solving problems and I'm always seeking improvement in the work I do.<br/> 
       Nevertheless, I have a curious mind which often leads me to want to learn more about how tools work under the hood.</p>
-      <a target='_blank' href="https://mail.google.com/mail/u/0/?fs=1&to=amruser97@gmail.com&su=SUBJECT&body=BODY&tf=cm" className={classes}>Get In Touch</a>
+      <a target='_blank' href="https://mail.google.com/mail/u/0/?fs=1&to=amruser97@gmail.com&tf=cm" className={classes}>Get In Touch</a>
      </article>
     </section>
     )
