293f0ac551da5923542b8b04c14543dc85a99103
Amr <amruser97@gmail.com>
2023-06-11 07:55:39 +0300

    updated about section

--- src/components/about.js
+++ src/components/about.js
@@ -11,7 +11,7 @@ export default function About() {
         <article>
           <h2>About</h2>
           <p className={classes}>
-            I am a Web Developer Located In Egypt. May describe myself as an
+            {/* I am a Web Developer Located In Egypt. May describe myself as an
             organized person, highly enthusiastic about problem-solving with
             high attention to detail, Also a Family person who loves animals
             -especially birds-, reading and point and click adventure/puzzle
@@ -24,7 +24,13 @@ export default function About() {
             Although I find everything about programming interesting, web
             development is my passion and I love working in all of its areas :
             Front-End, Back-End, Databases and even Security, My experience
-            mostly spans the Javascript-ecosystem.
+            mostly spans the Javascript-ecosystem. */}
+            I am an Egyptian based Software Developer, One of my strongest
+            ambitions is to become the best programmer I can humanly be.
+            Although I find everything about programming interesting, web
+            development is my passion and I enjoy facing challenges across the
+            entire stack. <br /> <br /> Hobbies beside programming: Reading,
+            Sports and Low combat narrative/puzzle heavy Games.
           </p>
           <a
             className={classes}
